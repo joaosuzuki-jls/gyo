91122b1c1287bd5492b03cdb432a2ca89af6201c
joao-suzuki-jls <joao.lucas@mewti.com>
2025-03-31 08:51:51 -0300

    test

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ const canvas = document.getElementById("canvas-grid");
 const startButton = document.getElementById('start-button');
 const ctx = canvas.getContext('2d');
 const cellSize = 20;
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
 
-for(let i=0; i <= 400; i += cellSize){
+
+for(let i=0; i <= innerWidth; i += cellSize){
   ctx.fillStyle = 'grey';
-  ctx.fillRect(0, i, 400, 1);
-  ctx.fillRect(i, 0, 1, 400);
+  ctx.fillRect(0, i, ctx.canvas.width, 1);
+  ctx.fillRect(i, 0, 1, ctx.canvas.height);
 }
 
 function StartSimulation() {
-  //
 }
 
-function GetCursorPosition(canvas, event) {
+function GetCellPosition(canvas, event) {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
@@ -27,16 +29,16 @@ function GetCursorPosition(canvas, event) {
 function DrawLiveCell(canvasX, canvasY) {
   const xSquarePos = Math.trunc(canvasX);
   const ySquarePos = Math.trunc(canvasY); 
-  
   ctx.fillStyle = 'black';  
-  ctx.fillRect(xSquarePos * cellSize, ySquarePos * cellSize, cellSize, cellSize);
+  ctx.fillRect(xSquarePos * cellSize, ySquarePos * cellSize, cellSize, cellSize);    
 }
 
 canvas.addEventListener('mouseup', (event) => {
-  const positions = GetCursorPosition(canvas, event);
+  const positions = GetCellPosition(canvas, event);
   DrawLiveCell(positions[0], positions[1]);
-});
+  }
+);
 
 startButton.addEventListener('click', () => {
-  SimulationStart();
+  StartSimulation();
 })
